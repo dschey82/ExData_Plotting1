a853ed5fa0ea917ed6b8c56371cd5619842b0f57
dan schey <dan.schey@gmail.com>
2016-02-04 23:31:06 -0800

    plots

---
+++ plot1.R
@@ -0,0 +1,22 @@
+old_dir <- getwd()
+setwd("c:/users/dansc/documents/coursera/exploratory data analysis/project1")
+if (!file.exists("exdata-data-household_power_consumption.zip")) {
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                    destfile="exdata-data-household_power_consumption.zip")
+}
+unzip("exdata-data-household_power_consumption.zip")
+
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+dated <- subset(data, Date >= as.Date("2007-02-01", "%Y-%m-%d") & Date <= as.Date("2007-02-02", "%Y-%m-%d"))
+temp <- paste("2007-02-01", as.character(dated[dated$Date == as.Date("2007-02-01"), ]$Time))
+times <- strptime(temp, format = "%Y-%m-%d %H:%M:%S")
+dated$Time <- times
+dated$Global_active_power <- as.numeric(levels(dated$Global_active_power)[dated$Global_active_power])
+
+par(mfrow=c(1,1))
+hist(dated$Global_active_power, col="red", xlab = "Global active power (kilowatts)",
+        ylab  = "Frequency", main = "Global Active Power")
+dev.copy(png, "plot1.png")
+dev.off()
+setwd(old_dir)
---
+++ plot4.R
@@ -0,0 +1,32 @@
+old_dir <- getwd()
+setwd("c:/users/dansc/documents/coursera/exploratory data analysis/project1")
+if (!file.exists("exdata-data-household_power_consumption.zip")) {
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                    destfile="exdata-data-household_power_consumption.zip")
+}
+unzip("exdata-data-household_power_consumption.zip")
+
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+dated <- subset(data, Date >= as.Date("2007-02-01", "%Y-%m-%d") & Date <= as.Date("2007-02-02", "%Y-%m-%d"))
+temp <- paste(dated$Date, as.character(dated$Time))
+times <- strptime(temp, format = "%Y-%m-%d %H:%M:%S")
+dated$Time <- times
+dated$Global_active_power <- as.numeric(levels(dated$Global_active_power)[dated$Global_active_power])
+dated$Global_reactive_power <- as.numeric(levels(dated$Global_reactive_power)[dated$Global_reactive_power])
+dated$Voltage <- as.numeric(levels(dated$Voltage)[dated$Voltage])
+dated$Global_intensity <- as.numeric(levels(dated$Global_intensity)[dated$Global_intensity])
+dated$Sub_metering_1 <- as.numeric(levels(dated$Sub_metering_1)[dated$Sub_metering_1])
+dated$Sub_metering_2 <- as.numeric(levels(dated$Sub_metering_2)[dated$Sub_metering_2])
+
+par(mfrow=c(2,2))
+plot(dated$Time, dated$Global_active_power, xlab="", ylab="Global Active Power", type="l")
+plot(dated$Time, dated$Voltage, xlab="datetime", ylab="Voltage", type="l")
+plot(dated$Time, dated$Sub_metering_1, xlab="", ylab="Energy sub metering", type="l", col="black")
+lines(dated$Time, dated$Sub_metering_2, col="red")
+lines(dated$Time, dated$Sub_metering_3, col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black","red","blue"), lwd=1, bty="n")
+plot(dated$Time, dated$Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", type="l")
+dev.copy(png, "plot4.png")
+dev.off()
+setwd(old_dir)
